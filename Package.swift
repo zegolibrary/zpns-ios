56928fc6b401c2919debb6336bd132d0be6a1368
zegolibrary <developer@zego.im>
2023-02-06 17:28:48 +0800

    🔖 Release 0.0.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZPNs",
-            url: "",
-            checksum: ""
+            url: "https://artifact-node.zego.cloud/generic/swiftpm/public/zpns-ios/ZPNs.xcframework.zip?version=0.0.2",
+            checksum: "8e7536477aff244099068b49194ca95c169480e38a67a02a3945fb78eb77529d"
         )
     ]
 )
